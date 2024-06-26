fc1a9c61c4adf7bf59777f36ca060e3a54504cf5
noa <snoachi@yahoo.co.jp>
2024-06-26 15:17:32 +0900

    初期コミットに不具合があったため修正

---
+++ src/main/java/com/example/demo/EasyReserveKunApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyReserveKunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyReserveKunApplication.class, args);
+	}
+
+}
