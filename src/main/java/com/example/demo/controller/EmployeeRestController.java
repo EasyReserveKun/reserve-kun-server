e87201ee0d817d1f1c1f23850b5f7aa14209535c
noa <snoachi@yahoo.co.jp>
2024-07-01 11:28:37 +0900

    ファイル構造を修正

--- src/main/java/com/example/demo/controller/EmployeeRestController.java
+++ src/main/java/com/example/demo/controller/EmployeeRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Employee;
-import com.example.demo.entity.repository.EmployeeRepository;
+import com.example.demo.repository.EmployeeRepository;
 
 import lombok.AllArgsConstructor;
 
