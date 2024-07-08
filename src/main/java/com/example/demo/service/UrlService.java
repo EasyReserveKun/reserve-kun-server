fc1822d1698a7afe9190370625f8b475b6cf0772
noa <snoachi@yahoo.co.jp>
2024-07-08 12:38:37 +0900

    URLの扱いについて変更

---
+++ src/main/java/com/example/demo/service/UrlService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+public class UrlService {
+	public static String getWebUrl(){
+		return "https://easy-reserve-kun.azurewebsites.net";
+		//return "https://localhost:3000";
+	}
+}
