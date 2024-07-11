6bbb1c44bb64d14ac76bb326ce0bf1d8a62ebedc
noa <snoachi@yahoo.co.jp>
2024-07-11 16:48:12 +0900

    CrossOriginを許容

--- src/main/java/com/example/demo/controller/QuestionAnsweringController.java
+++ src/main/java/com/example/demo/controller/QuestionAnsweringController.java
@@ -2,8 +2,11 @@ package com.example.demo.controller;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.CustomQuestionAnsweringService;
@@ -19,6 +22,12 @@ public class QuestionAnsweringController {
     @PostMapping("/search")
     public Map<String, Object> askQuestion(@RequestBody Map<String, String> request) {
         String question = request.get("question");
+        return cqaService.getAnswer(question);
+    }
+
+    @CrossOrigin
+    @GetMapping("/ask")
+    public Map<String, Object> askQuestion(@RequestParam String question) {
         return cqaService.getAnswer(question);
     }
 }
