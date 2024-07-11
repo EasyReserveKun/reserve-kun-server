4435243b400023311be48d0f93d32c4451e9edd1
noa <snoachi@yahoo.co.jp>
2024-07-11 16:24:39 +0900

    POSTに変更

--- src/main/java/com/example/demo/controller/QuestionAnsweringController.java
+++ src/main/java/com/example/demo/controller/QuestionAnsweringController.java
@@ -2,8 +2,8 @@ package com.example.demo.controller;
 
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.CustomQuestionAnsweringService;
@@ -16,8 +16,9 @@ public class QuestionAnsweringController {
 
     private final CustomQuestionAnsweringService cqaService;
 
-    @GetMapping("/ask")
-    public Map<String, Object> askQuestion(@RequestParam String question) {
+    @PostMapping("/search")
+    public Map<String, Object> askQuestion(@RequestBody Map<String, String> request) {
+        String question = request.get("question");
         return cqaService.getAnswer(question);
     }
 }
