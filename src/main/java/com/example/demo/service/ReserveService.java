059e5ef2b4bcfcee6a9d606c3b23b2fa8a4f7f21
Suzuki20010319 <yota.s130319@icloud.com>
2024-07-19 14:30:51 +0900

    停止機能の実装

--- src/main/java/com/example/demo/service/ReserveService.java
+++ src/main/java/com/example/demo/service/ReserveService.java
@@ -4,12 +4,8 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Customer;
 import com.example.demo.entity.Reserve;
-import com.example.demo.form.CustomerForm;
-import com.example.demo.form.CustomerLoginForm;
 import com.example.demo.form.ReserveForm;
-import com.example.demo.repository.CustomerRepository;
 import com.example.demo.repository.ReserveRepository;
 
 import lombok.AllArgsConstructor;
@@ -25,6 +21,27 @@ public class ReserveService {
 				reserveForm.getDate(), reserveForm.getTime(), reserveForm.getEid());
 		Optional<Reserve> doubled = reserveRepository.findAllByDateAndTimeAndCid(
 				reserveForm.getDate(), reserveForm.getTime(), reserveForm.getCid());
+		System.out.println(reserveForm.getDate());
+		System.out.println(reserveForm.getTime());
+		System.out.println(reserveForm.getCid());
+
+		if (!duplicated.isEmpty()) {
+			return "Duplicated";
+		}
+		if (!doubled.isEmpty()) {
+			return "Doubled";
+		}
+		return "";
+	}
+	
+	public String reserveEmployeeCheck(ReserveForm reserveForm) {
+		Optional<Reserve> duplicated = reserveRepository.findAllByDateAndTimeAndEid(
+				reserveForm.getDate(), reserveForm.getTime(), reserveForm.getEid());
+		Optional<Reserve> doubled = reserveRepository.findAllByDateAndTimeAndCid(
+				reserveForm.getDate(), reserveForm.getTime(), reserveForm.getEid());
+		System.out.println(reserveForm.getDate());
+		System.out.println(reserveForm.getTime());
+		System.out.println(reserveForm.getCid());
 
 		if (!duplicated.isEmpty()) {
 			return "Duplicated";
