a225c938dedf6433081dcc16129faee573b2680d
noa <snoachi@yahoo.co.jp>
2024-07-19 11:34:33 +0900

    Transactionalがついていない為Exceptionが出る問題を修正

--- src/main/java/com/example/demo/service/ScheduleService.java
+++ src/main/java/com/example/demo/service/ScheduleService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import com.example.demo.config.ExpireMinutesConfig;
 import com.example.demo.repository.TemporaryRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
 @Component
@@ -15,6 +16,7 @@ public class ScheduleService {
 	private final TemporaryRepository temporaryRepository;
 	private final ExpireMinutesConfig emConfig;
 	
+	@Transactional
 	@Scheduled(cron = "0 0,30 * * * *")
     public void scheduledDeleteExpire() {
         temporaryRepository.deleteExpire(emConfig.getCodeExpiryMinutesString());
