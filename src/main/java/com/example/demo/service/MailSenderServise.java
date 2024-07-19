2234ef7f1033b90be458c690fec4f2a9f6dd23fc
noa <snoachi@yahoo.co.jp>
2024-07-19 09:43:53 +0900

    メール送信をAsync化

--- src/main/java/com/example/demo/service/MailSenderServise.java
+++ src/main/java/com/example/demo/service/MailSenderServise.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
-import org.springframework.mail.MailSender;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -10,18 +11,19 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class MailSenderServise {
 
-    private final JavaMailSender emailSender;
+	private final JavaMailSender emailSender;
 
-    public void sendSimpleMessage(String to, String subject, String text) {
-        var mail = new SimpleMailMessage();
-        mail.setTo(to);
-        mail.setFrom("mailuser_semi847@orange-piece2.sakura.ne.jp");
-        mail.setSubject(subject);
-        mail.setText(text);
-        try {
-        emailSender.send(mail);
-        } catch(Exception e) {
-        	System.out.println(e);
-        }
-    }
+	@Async
+	public void sendSimpleMessage(String to, String subject, String text) {
+		var mail = new SimpleMailMessage();
+		mail.setTo(to);
+		mail.setFrom("mailuser_semi847@orange-piece2.sakura.ne.jp");
+		mail.setSubject(subject);
+		mail.setText(text);
+		try {
+			emailSender.send(mail);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
