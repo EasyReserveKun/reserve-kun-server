0fada30ed76deb665e396d3687198284e466246a
noa <snoachi@yahoo.co.jp>
2024-07-30 10:16:22 +0900

    不要なimportの削除

--- src/main/java/com/example/demo/controller/QuestionAnsweringController.java
+++ src/main/java/com/example/demo/controller/QuestionAnsweringController.java
@@ -3,10 +3,8 @@ package com.example.demo.controller;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.CustomQuestionAnsweringService;
